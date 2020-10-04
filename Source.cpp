82d4c301b8b6899507d24af3cf103498a9223c91
Radu Enachi <34218173+Parad0xF@users.noreply.github.com>
2020-10-04 12:00:23 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -36,36 +36,54 @@ int convertToInt(char a) {
 	return stoi(&a);
 }
 
-int evaluatePostfix(string string) {
-	
-	Stack<char> a;
-	int result =0;
+int evaluatePostfix(string string) throw(logic_error) {
 
-	for (int i =0 ; i <string.size(); i++) {
-		//Check if the element string[i] is digit.
-		if (string[i] >= '0' && string[i] <= '9') {
-
-			a.push(convertToInt(string[i]));
-		}
-		else
-		{
-			//Evaluate and push the results
-			int operand2 = a.peak();
-			a.pop();
-
-			int operand1 = a.peak();
-			a.pop();
-
-			result = compute(operand1, operand2, string[i]);
-			a.push(result);
+	try {
+		int d = 1;
+		
+		cout << "checks " << d << endl;
+		d++;
+		for (auto i=0; i < string.size(); i++) {
+			if (string[i] <= 'z' && string[i] >= 'a') {
+				throw logic_error("The string is not PostFix format!");
+			}
 		}
 	}
+	catch (logic_error lgre) {
+
+		cout<<  lgre.what() << endl;
+
+	}
+
+
+	Stack<char> a;
+	int result =0;
+	
+		for (int i = 0; i < string.size(); i++) {
+			//Check if the element string[i] is digit.
+			if (string[i] >= '0' && string[i] <= '9') {
+
+				a.push(convertToInt(string[i]));
+			}
+			else
+			{
+				//Evaluate and push the results
+				int operand2 = a.peak();
+				a.pop();
+
+				int operand1 = a.peak();
+				a.pop();
+
+				result = compute(operand1, operand2, string[i]);
+				a.push(result);
+			}
+		}
 
 	return result;
 }
 int main() {
 
-	string ch = "234+*";
+	string ch = "aaa";
 	int result = evaluatePostfix(ch);
 
 	cout << result << endl;
